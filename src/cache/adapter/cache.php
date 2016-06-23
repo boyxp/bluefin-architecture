244d8100d71015cfc08268ecd7f28021fe968b86
boyxp <isme@live.com>
2016-06-23 10:53:21 +0800

    构件指定默认实现

---
+++ src/cache/adapter/cache.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+namespace bluefin\architecture\cache\adapter;
+class cache extends redis
+{
+}
---
+++ src/queue/adapter/queue.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+namespace bluefin\architecture\queue\adapter;
+class queue extends redis
+{
+}
